43faa25845d6d6a04ca9d1b1f587b110765dccfc
Sair Barreto <sairgregori@gmail.com>
2022-10-16 17:45:01 -0300

    Agregue el modulo menu en el main.cpp

--- main.cpp
+++ main.cpp
@@ -3,11 +3,22 @@
 
 using namespace std;
 
+void menu();
+
 int main()    
 {
-    string nombre = "Sair";
-
-    cout << "Hola " << nombre << endl;
+    menu();
 
     return 0;
+}
+
+void menu()
+{
+    cout << "\tMenu" << endl;
+    cout << "1.Listar animales" << endl;
+    cout << "2.Rescatar animal" << endl;
+    cout << "3.Buscar animal" << endl;
+    cout << "4.Cuidar animales" << endl;
+    cout << "5.Adoptar animal" << endl;
+    cout << "6.Guardar y salir" << endl;
 }
