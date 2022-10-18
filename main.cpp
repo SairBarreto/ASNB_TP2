ceec576e9d67da71c99484554305a28217d1495f
Sair Barreto <sairgregori@gmail.com>
2022-10-18 13:11:17 -0300

    Agregue los archivos para el menu

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include "menu.h"
 
 using namespace std;
 
@@ -11,23 +12,4 @@ int main()
     menu();
 
     return 0;
-}
-
-int opcion_elegida()
-{
-    int opcion;
-
-    cout << "Opcion: ";
-    cin >> opcion;
-
-    return opcion;
-void menu()
-{
-    cout << "\tMenu" << endl;
-    cout << "1.Listar animales" << endl;
-    cout << "2.Rescatar animal" << endl;
-    cout << "3.Buscar animal" << endl;
-    cout << "4.Cuidar animales" << endl;
-    cout << "5.Adoptar animal" << endl;
-    cout << "6.Guardar y salir" << endl;
 }
