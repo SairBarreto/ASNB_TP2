029e02b278c323ff2de4df7a039f976957eac849
Bauti Corti <bautistamariacorti@gmail.com>
2022-10-30 12:27:15 -0300

    Cambié main,menu para el merge

--- main.cpp
+++ main.cpp
@@ -1,23 +1,14 @@
 #include <iostream>
 #include <string>
-
-#include "lista.h"
-#include "animal.h"
+#include "menu.h"
 
 using namespace std;
 
+void menu();
+
 int main()    
 {
-    Lista<Animal> *l = new Lista<Animal>;
-
-    Animal* gato = new Animal("membrillo", 1, "chico");
-
-    l->agregar_nodo(gato);
-    Animal* perro = l->obtener_actual_dato();
-    cout << perro->mostrar_nombre() << endl;
-    l->borrar_nodo_actual();
-
-    delete l;
+    menu();
 
     return 0;
 }
